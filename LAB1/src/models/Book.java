527e9e4f4f83e989f8442980c340b5f9f0c2acf0
Garbuz Nelli <120444803+nelldino@users.noreply.github.com>
2024-10-13 22:14:11 +0300

    delete unused methods

--- LAB1/src/models/Book.java
+++ LAB1/src/models/Book.java
@@ -7,7 +7,6 @@ public abstract class Book {
     public int bookQuantity;
     public int bookQuantityCopy;
 
-    // Constructor
     public Book(String title, String author, String ISBN, int bookQuantity) {
         this.title = title;
         this.author = author;
@@ -16,27 +15,5 @@ public abstract class Book {
         this.bookQuantityCopy = bookQuantity;
     }
 
-    // Getters for the book properties
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getISBN() {
-        return ISBN;
-    }
-
-    public int getBookQuantity() {
-        return bookQuantity;
-    }
-
-    public int getBookQuantityCopy() {
-        return bookQuantityCopy;
-    }
-
-    // Abstract method to define the type of book
     public abstract String getType();
 }
